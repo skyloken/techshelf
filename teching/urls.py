9048fe608999fbe6ceaa400fb579b7a56432aaf0
skyloken <risora1234@gmail.com>
2019-04-21 18:42:14 +0900

    refs #3 indexページの作成，UIKitの導入

--- teching/urls.py
+++ teching/urls.py
@@ -17,10 +17,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('app.urls'))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
