997c82b554a21a4cba4b7deb75a4c649b6e96183
Kenta Tanigaki <risora1234@gmail.com>
2019-05-08 02:19:10 +0900

    ユーザアイコンのデフォルトを設定

--- users/models.py
+++ users/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 
 class User(AbstractUser):
-    icon = models.ImageField('アイコン', upload_to='icons/', blank=True, null=True)
+    icon = models.ImageField('アイコン', upload_to='icons/', default='icons/default.png')
     bio = models.CharField('バイオグラフィー', max_length=200, blank=True, null=True)
