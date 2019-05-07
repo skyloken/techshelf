6fc8b6abfeb2b744bb133acbd06ff70327a5c745
Kenta Tanigaki <risora1234@gmail.com>
2019-05-08 01:36:24 +0900

    add log setting

--- teching/settings.py
+++ teching/settings.py
@@ -128,6 +128,24 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+# Log settings
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 # local settings
 try:
     from .local_settings import *
