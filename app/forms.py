a2a4b1da0ecd970f63017475613d4a69d43e4a85
Kenta Tanigaki <risora1234@gmail.com>
2019-05-12 19:51:07 +0900

    レビュー投稿機能を実装

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django_starfield import Stars
 
-from .models import Book
+from .models import Book, Review
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -13,6 +14,7 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class AddBookForm(forms.ModelForm):
+    """本追加フォーム"""
     isbn = forms.CharField(
         max_length=13,
         min_length=13,
@@ -28,3 +30,25 @@ class AddBookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ('isbn',)
+
+
+class PostReviewForm(forms.ModelForm):
+    """レビュー投稿フォーム"""
+
+    score = forms.IntegerField(
+        label='',
+        max_value=5.0,
+        min_value=1.0,
+        required=True,
+        widget=Stars
+    )
+
+    class Meta:
+        model = Review
+        fields = ('score', 'title', 'reason', 'body',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'class': 'uk-input'})
+        self.fields['reason'].widget.attrs.update({'class': 'uk-textarea', 'rows': 3})
+        self.fields['body'].widget.attrs.update({'class': 'uk-textarea', 'rows': 8})
--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib import messages
 
 from .models import Book, Review
-from .forms import CustomUserCreationForm, AddBookForm
+from .forms import CustomUserCreationForm, AddBookForm, PostReviewForm
 from .service import get_book_info
 from django.db.models import Avg
 
@@ -32,16 +33,33 @@ def books(request):
         if add_book_form.is_valid():
             # 新規本追加処理
             book = get_book_info(add_book_form.save(commit=False))
-            return redirect('book_detail', book_id=book.id)
-    else:
-        add_book_form = AddBookForm()
-        book_list = Book.objects.order_by('title').annotate(ave_score=Avg('review__score'))
-        context = {'books_page': 'active', 'book_list': book_list, 'add_book_form': add_book_form}
-        return render(request, 'app/books.html', context)
+            messages.success(request, '『{0}』を追加しました'.format(book.title))
+            return redirect('books')
+    add_book_form = AddBookForm()
+    book_list = Book.objects.order_by('title').annotate(ave_score=Avg('review__score'))
+    context = {'books_page': 'active', 'book_list': book_list, 'add_book_form': add_book_form}
+    return render(request, 'app/books.html', context)
 
 
 def book_detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
+    if request.method == "POST":
+        # レビュー登録処理
+        post_review_form = PostReviewForm(request.POST)
+        if post_review_form.is_valid():
+            review = post_review_form.save(commit=False)
+            review.user = request.user
+            review.book = book
+            review.save()
+            messages.success(request, 'レビューが投稿されました')
+        return redirect('book_detail', book_id=book_id)
     agg = book.review_set.aggregate(Avg('score'))
-    context = {'books_page': 'active', 'book': book, 'ave_score': agg['score__avg']}
+    post_review_form = PostReviewForm()
+    context = {
+        'books_page': 'active',
+        'book': book,
+        'reviews': book.review_set.order_by('-reviewed_at'),
+        'ave_score': agg['score__avg'],
+        'post_review_form': post_review_form
+    }
     return render(request, 'app/book_detail.html', context)
